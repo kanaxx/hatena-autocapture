ef8f230b438184910079f1c7f7bb017571ce431b
kanaxx <tsh258@gmail.com>
2022-09-20 22:57:01 +0900

    add run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+export PATH="$PATH:/home/$USER/nodejs/share/nodejs/node-v16.17.0-linux-x64/bin"
+export LANG="ja_JP.utf8"
+
+export blog_admin_url="https://blog.hatena.ne.jp/xxx/"
+export hatena_id=""
+export hatena_pass=""
+
+export CLOUDINARY_URL="cloudinary://xx:yy@zz"
+
+node hatena-autocapture.js
